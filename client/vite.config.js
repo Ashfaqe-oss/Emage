415ce9c999141a24f0838c56dc4f9627976c23df
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2023-01-22 19:01:07 +0530

    attempt pwa 1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react(), VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+        includeAssets: ['icon-192x192.png', 'icon-512x512.png'],
+        manifest: {
+            name: 'Emage',
+            short_name: 'Emage',
+            description: 'AI image genearation app',
+            theme_color: '#000',
+            icons: [{
+                    src: 'icon-192x192',
+                    sizes: '192x192',
+                    type: 'image/png'
+                },
+                {
+                    src: 'icon-512x512',
+                    sizes: '512x512',
+                    type: 'image/png'
+                }
+            ],
+            // start_url: '/'
+        }
+    })],
+    define: {
+        global: "globalThis",
+        "process.env": {},
+    },
+});
